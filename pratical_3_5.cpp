256adcb98c90beb3bcfdaeba2b0cebeb864c987a
kavyamrutiya <kavy.amrutiya@gmail.com>
2025-04-13 21:43:31 +0530

    Add files via upload

---
+++ pratical_3_5.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int SuperDigit(long long Number, int z){
+    if(z==1) 
+    return Number;
+    else{
+        long long Super =0;
+        int y=0;
+        
+        while(Number>=1){
+            Super+= Number%10; 
+            Number/=10;
+            y++;
+        }
+
+        return SuperDigit(Super, y); 
+    }
+}
+
+int main()
+{
+    string Number, N;
+    int k;
+
+    cout<<"\n\nEnter the Number: ";cin>>N;
+    cout<<"Enter the number of concatenations of this number: ";cin>>k;
+
+    for(int i=0; i<k; i++){
+        Number+=N;
+    }
+
+    cout<<"\nThe Super Digit of "<<Number<<" is "<<SuperDigit(stoll(Number), Number.size()); 
+    
+    return 0;
+}
