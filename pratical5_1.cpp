3237264611251a91b93865cf40fa981fbf03a69a
kavyamrutiya <kavy.amrutiya@gmail.com>
2025-04-20 17:35:05 +0530

    Update pratical5_1.cpp

--- pratical5_1.cpp
+++ pratical5_1.cpp
@@ -1,47 +1,45 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-class calculator
+class Calculator
 {
-    int number1,number2;
-    float fnumber1,fnumber2;
     public:
-    int addNumber(int num1,int num2)
+    int Add_numbers(int num1, int num2)
     {
-        return num1 + num2;
+        return num1+num2;
     }
-    float addNumber(float fnum1 , float fnum2)
+    float Add_numbers(float fnum1,float fnum2)
     {
         return fnum1+fnum2;
     }
 };
 int main()
 {
-    calculator cal;
-    int choice,num1,num2,answer;
-    float fnum1,fnum2,fanswer;
-    cout<<"Enter 1 to add integer"<<endl;
-    cout<<"Enter 2 to add floating number"<<endl;
-    cout<<"Enter your choice: ";
+    Calculator Cal;
+    int choice, number1 , number2 ,answer;
+    float fnumber1,fnumber2,fanswer;
+    cout<<"Enter 1 to add integers"<<endl;
+    cout<<"Enter 2 to add floating point numbers"<<endl;
+    cout<<"Enter your choice : ";
     cin>>choice;
-    switch(choice)
-    {
+    switch(choice){
         case 1:
-        cout<<"Enter 1st number: ";
-        cin>>num1;
-        cout<<"Enter 2nd number: ";
-        cin>>num2;
-        answer=cal.addNumber(num1,num2);
-        cout<<"Sum of 2 integer is: "<<answer<<endl;
+        cout<<"Enter 1st number : ";
+        cin>>number1;
+        cout<<"Enter 2nd number : ";
+        cin>>number2;
+        answer=Cal.Add_numbers(number1,number2);
+        cout<<"Sumation of 2 integer number is "<<answer<<endl;
         break;
         case 2:
-        cout<<"Enter 1st number: ";
-        cin>>fnum1;
-        cout<<"Enter 2nd number: ";
-        cin>>fnum2;
-        fanswer=cal.addNumber(fnum1,fnum2);
-        cout<<"Sum of 2 floating number is: "<<fanswer<<endl;
+        cout<<"Enter 1st number : ";
+        cin>>fnumber1;
+        cout<<"Enter 2nd number : ";
+        cin>>fnumber2;
+        fanswer=Cal.Add_numbers(fnumber1,fnumber2);
+        cout<<"Sumation of 2 floating-point number is "<<fanswer<<endl;
         break;
-        default:
+        default: 
         cout<<"INVALID CHOICE";
+
     }
 }
