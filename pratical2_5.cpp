f512996d14dd9ca1bacf88a81fcd6ed9d028e720
kavyamrutiya <kavy.amrutiya@gmail.com>
2025-04-20 17:29:03 +0530

    Update pratical2_5.cpp

--- pratical2_5.cpp
+++ pratical2_5.cpp
@@ -2,49 +2,74 @@
 #include <cmath>
 using namespace std;
 
-class loan_information {
-    int loan_id;
-    string appl_name;
-    int time_period;  
-    int total_loan_amount;
-    float annual_interest;
-    float emi;
-
-    void emi_calculator(int amount, int time, float interest) {
+class Loan_Info
+{
+    int Loan_id;
+    string Applicant_name;
+    int Time_period;
+    int Total_loan_amount;
+    double Emi;
+    float Anual_intrest;
+    void Emi_calculator(int amount ,int time , float intrest)
+    {
+       
+        Emi = (amount*intrest*pow((1.0+intrest) , time))/(pow((1.0+intrest) , time) - 1);
         
-        float R = interest / 12 / 100;  
-        emi = (amount * R * pow(1 + R, time)) / (pow(1 + R, time) - 1); 
     }
-
-public:
-    void get_details() {
-        cout << "Enter your loan id: ";
-        cin >> loan_id;
-        cout << "Enter your name: ";
-        cin >> appl_name;
-        cout << "Enter your time period (in months): ";
-        cin >> time_period;
-        cout << "Enter your total loan amount: ";
-        cin >> total_loan_amount;
-        cout << "Enter your annual interest rate (%): ";
-        cin >> annual_interest;
+    public:
+    void Add_loan()
+    {
+        cout<<"Enter Loan ID : ";
+        cin>>Loan_id;
+        cout<<"Enter Applicant Name : ";
+        cin>>Applicant_name;
+        cout<<"Enter total loan amount : ";
+        cin>>Total_loan_amount;
+        cout<<"Enter tenure of loan : ";
+        cin>> Time_period;
+        cout<<"Enter Loan Interst : ";
+        cin>>Anual_intrest;
+        Emi_calculator(Time_period , Total_loan_amount , Time_period);
     }
+    void Display_Details()
+    {
+        cout<<"Loan ID : "<<Loan_id<<endl;
+        cout<<"Applicant's Name : "<<Applicant_name<<endl;
+        cout<<"Emi : "<<Emi<<endl;
 
-    void display_details() {
-        
-        emi_calculator(total_loan_amount, time_period, annual_interest);
-        cout << "Loan ID: " << loan_id << endl;
-        cout << "Applicant's name: " << appl_name << endl;
-        cout << "Loan time period (in months): " << time_period << endl;
-        cout << "Total loan amount: " << total_loan_amount << endl;
-        cout << "Annual interest rate: " << annual_interest << "%" << endl;
-        cout << "Your EMI is: " << emi << endl;
     }
 };
 
-int main() {
-    loan_information loan;
-    loan.get_details();
-    loan.display_details();
+
+int main()
+{
+    Loan_Info lf[50];
+    int Choice , Loan_count=0;
+    do
+    {
+        cout<<"Enter 1 to add Loan details"<<endl;
+        cout<<"Enter 2 to display detials"<<endl;
+        cout<<"Enter 3 to EXIT"<<endl;
+        cout<<"Enter your Choice : ";
+        cin>>Choice;
+        switch(Choice)
+        {
+            case 1 :
+                lf[Loan_count].Add_loan();
+                Loan_count++;
+                break;
+            case 2:
+                for(int i=0 ; i<Loan_count ; i++)
+                {
+                    lf[i].Display_Details();
+                }
+                break;
+            case 0:
+                cout<<"THANKS FOR VISITING"<<endl;
+            default :
+                cout<<"INVALID : CHOICE"<<endl;
+                break;
+        }
+    }while(Choice != 0);
     return 0;
 }
