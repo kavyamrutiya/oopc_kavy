9cefabc682690e2e25c6f9510dcc1e333ae24f1a
kavyamrutiya <kavy.amrutiya@gmail.com>
2025-04-20 17:33:30 +0530

    Update problem4_2.cpp

--- problem4_2.cpp
+++ problem4_2.cpp
@@ -7,10 +7,10 @@ class Person
     string Name;
     protected:
     Person(){}
-    Person(int age , string name)
+    Person(int Age , string Name)
     {
-        Age = age;
-        Name = name;
+        this->Age = Age;
+        this->Name = Name;
     }
     void Display_person_details()
     {
@@ -23,13 +23,13 @@ class Employee:private Person
     int ID;
     public:
     Employee(){}
-    Employee(int age,string name,int id):Person(age,name)
+    Employee(int Age,string Name,int Id):Person(Age,Name)
     {
-        ID = id;
+        ID = Id;
     }
-    void Display_Employee_Details()
+    void Display_employee_details()
     {
-        cout<<"ID : "<<ID<<endl;
+        cout<<"\nID : "<<ID<<endl;
         Display_person_details();
     }
 };
@@ -38,13 +38,13 @@ class Manager:private Employee
     string Department;
     public:
     Manager(){}
-    Manager(int age , string name , int id , string department):Employee(age,name,id)
+    Manager(int Age , string Name , int Id , string Department):Employee(Age,Name,Id)
     {
-        Department = department;
+       this->Department = Department;
     }
     void Display_manager_details()
     {
-        Display_Employee_Details();
+        Display_employee_details();
         cout<<"Department :"<<Department<<endl;
     }
 };
@@ -53,49 +53,49 @@ int main()
     vector<Employee>Emp;
     vector<Manager>Man;
     Employee emp;
-    int Employee_count=0,Manager_count=0,choice,age,id;
-    string department,name;
+    int Employee_count=0,ManAger_count=0,Choice,Age,Id;
+    string Department,Name;
     do
     {
-        cout<<"Enter 1 to add Employee"<<endl;
+        cout<<"\nEnter 1 to add Employee"<<endl;
         cout<<"Enter 2 to add Manager"<<endl;
         cout<<"Enter 3 to display Employess"<<endl;
         cout<<"Enter 4 to display Manager"<<endl;
         cout<<"Enter 0 to exit"<<endl;
-        cout<<"Enter your choice : ";
-        cin>>choice;
-        switch(choice)
+        cout<<"Enter your Choice : ";
+        cin>>Choice;
+        switch(Choice)
         {
             case 1:
-                cout<<"Enter ID :";
-                cin>>id;
-                cout<<"Enter Name : ";
-                cin>>name;
-                cout<<"Enter Age : ";
-                cin>>age;
-                Emp.push_back(Employee(age,name,id));
+                cout<<"Enter your ID :";
+                cin>>Id;
+                cout<<"Enter your Name : ";
+                cin>>Name;
+                cout<<"Enter your  Age : ";
+                cin>>Age;
+                Emp.push_back(Employee(Age,Name,Id));
                 Employee_count++;
                 break;
             case 2:
-                cout<<"Enter ID :";
-                cin>>id;
-                cout<<"Enter Name : ";
-                cin>>name;
-                cout<<"Enter Age : ";
-                cin>>age;
-                cout<<"Enter department : ";
-                cin>>department;
-                Man.push_back(Manager(age,name,id,department));
-                Manager_count++;
+                cout<<"Enter your ID :";
+                cin>>Id;
+                cout<<"Enter your Name : ";
+                cin>>Name;
+                cout<<"Enter your Age : ";
+                cin>>Age;
+                cout<<"Enter your Department : ";
+                cin>>Department;
+                Man.push_back(Manager(Age,Name,Id,Department));
+                ManAger_count++;
                 break;
             case 3:
                 for(int i=0;i<Employee_count;i++)
                 {
-                    Emp[i].Display_Employee_Details();
+                    Emp[i].Display_employee_details();
                 }
                 break;
             case 4:
-                for(int i=0;i<Manager_count;i++)
+                for(int i=0;i<ManAger_count;i++)
                 {
                     Man[i].Display_manager_details();
                 }
@@ -106,5 +106,5 @@ int main()
             default:
                 cout<<"INVALID CHOICE"<<endl;
         }
-    }while(choice);
+    }while(Choice);
 }
