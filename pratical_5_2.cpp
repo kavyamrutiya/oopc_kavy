7474b9cbb6494a8c9f69b75b88b2eba1074a525c
kavyamrutiya <kavy.amrutiya@gmail.com>
2025-05-08 12:45:01 +0530

    Update pratical_5_2.cpp

--- pratical_5_2.cpp
+++ pratical_5_2.cpp
@@ -4,19 +4,19 @@ using namespace std;
 class Complex
 {
     public:
-    int Real_part, Imgnary_part;
+    int Real_part, Imagniary_part;
     void input()
     {
         cout<<"Enter num :";
-        cin>>Real_part>>Imgnary_part;
+        cin>>Real_part>>Imagniary_part;
 
     }
     void operator+(Complex &b)
     {
         Complex Ans;
         Ans.Real_part = Real_part + b.Real_part;
-        Ans.Imgnary_part= Imgnary_part+b.Imgnary_part;
-        cout<<Ans.Real_part<<"+"<<Ans.Imgnary_part<<"i";
+        Ans.Imagniary_part= Imagniary_part+b.Imagniary_part;
+        cout<<Ans.Real_part<<"+"<<Ans.Imagniary_part<<"i";
     }
 
 };
